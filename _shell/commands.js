5db8616ea772cb74f9ea83f98b5205163f539943
Shane Schulte <shane.schulte@samsara.com>
2021-04-06 00:33:18 -0700

    install + run prettier

--- _shell/commands.js
+++ _shell/commands.js
@@ -1,224 +1,240 @@
 module.exports = class CommandSet {
-    constructor(shell, fs) {
-        this.shell = shell;
-        this.fs = fs;
+  constructor(shell, fs) {
+    this.shell = shell;
+    this.fs = fs;
 
-        this.commands = {
-            "false": this.falseProgram,
-            "true": this.trueProgram,
-            "find": this.find,
-            "clear": this.clear,
-            "echo": this.echo,
-            "xargs": this.xargs,
-            "pwd": this.pwd,
-            "sudo": this.sudo,
-            "alias": this.alias,
-            "unalias": this.unalias,
-            "grep": this.grep,
-            "sarah.engineer": this.welcome,
-            "cat": this.cat,
-            "env": this.env,
-            "export": this.exportVar,
-            "whoami": this.whoami,
-            "curl": this.curl,
-            "ls": this.ls,
-            "cd": this.cd,
-            /** pity points */
-            "bash": this.sad,
-            "zsh": this.sad,
-            "sh": this.sad,
-            "fish": this.sad,
-            "chsh": this.sad,
-        };
+    this.commands = {
+      false: this.falseProgram,
+      true: this.trueProgram,
+      find: this.find,
+      clear: this.clear,
+      echo: this.echo,
+      xargs: this.xargs,
+      pwd: this.pwd,
+      sudo: this.sudo,
+      alias: this.alias,
+      unalias: this.unalias,
+      grep: this.grep,
+      "sarah.engineer": this.welcome,
+      cat: this.cat,
+      env: this.env,
+      export: this.exportVar,
+      whoami: this.whoami,
+      curl: this.curl,
+      ls: this.ls,
+      cd: this.cd,
+      /** pity points */
+      bash: this.sad,
+      zsh: this.sad,
+      sh: this.sad,
+      fish: this.sad,
+      chsh: this.sad,
+    };
+  }
+
+  grep = async (...args) => {
+    let invert = false;
+    if (args.includes("-v")) {
+      invert = true;
     }
-
-    grep = async (...args) => {
-        let invert = false;
-        if(args.includes('-v'))
-        {
-            invert = true;
-        }
-        args = args.filter(arg => arg !== '-v');
-        let re = new RegExp(args[0] || '', 'i')
-        if (!this.shell.stdin) {
-            this.shell.stdin = await readFileAsync(this.shell.cwd_p, args[1]);
-        }
-        return this.shell.stdin.split('\n').filter(line => invert ^ re.test(line)).join('\n');
+    args = args.filter((arg) => arg !== "-v");
+    let re = new RegExp(args[0] || "", "i");
+    if (!this.shell.stdin) {
+      this.shell.stdin = await readFileAsync(this.shell.cwd_p, args[1]);
     }
+    return this.shell.stdin
+      .split("\n")
+      .filter((line) => invert ^ re.test(line))
+      .join("\n");
+  };
 
-    cd = async (dir) => {
-        let inode = this.fs.findFileNode(this.shell.cwd_p, dir);
-        if(!inode || inode.isFile) {
-            this.shell.returnCode = 1
-            return "directory not found\n"
-        }
-        this.shell.cwd_p = inode;
-        // build new cwd
-        this.shell.cwd = inode.name
-        while(inode.parent) {
-            this.shell.cwd = inode.parent.name + this.shell.cwd;
-            inode = inode.parent;
-        }
-        return '';
+  cd = async (dir) => {
+    let inode = this.fs.findFileNode(this.shell.cwd_p, dir);
+    if (!inode || inode.isFile) {
+      this.shell.returnCode = 1;
+      return "directory not found\n";
     }
-
-    find = async (...args) => {
-        const find_r = (path) => (inode) => {
-            if(!inode.children) {
-                return [path + inode.name];
-            }
-            return [path + inode.name].concat(Object.values(inode.children).map(find_r(path + inode.name)));
-        }
-        let fileFilter = false;
-        let dirFilter = false;
-        while((args[0] || '').startsWith('-')) {
-            const flag = args.shift();
-            switch(flag) {
-                case '-type':
-                    const typeFilter = args.shift();
-                    if(typeFilter === 'f') {
-                        fileFilter = true;
-                    }
-                    else if(typeFilter === 'd') {
-                        dirFilter = true;
-                    }
-                    else {
-                        this.shell.returnCode = 1;
-                        throw new Error('unknown type flag\n');
-                    }
-                break;
-            }
-        }
-        let inode = this.fs.findFileNode(this.shell.cwd_p, args[0]);
-        if(!inode || inode.isFile) {
-            this.shell.returnCode = 1
-            return "directory not found\n"
-        }
-        const base = fileFilter ? [] : ['.'];
-        return base.concat(
-            find_r('')(inode).flat(Infinity)
-            .filter(file => (!fileFilter && !dirFilter) || (fileFilter && !file.endsWith('/') || (dirFilter && file.endsWith('/')))))
-            .join('\n');
+    this.shell.cwd_p = inode;
+    // build new cwd
+    this.shell.cwd = inode.name;
+    while (inode.parent) {
+      this.shell.cwd = inode.parent.name + this.shell.cwd;
+      inode = inode.parent;
     }
+    return "";
+  };
 
-    ls = async (dir) => {
-        let inode = this.fs.findFileNode(this.shell.cwd_p, dir);
-        if(!inode || inode.isFile) {
-            this.shell.returnCode = 1
-            return "directory not found\n"
-        }
-        const base = ['.'];
-        if (inode.parent) base.push('..');
-        return base.concat(Object.keys(inode.children)).join('\n');
-    }
-
-    curl = async (url) => {
-        const resp = await window.fetch(url);
-        return await resp.text();
-    }
-
-    trueProgram = () => {
-        this.shell.returnCode = 0
-        return ''
-    }
-
-    falseProgram = () => {
-        this.shell.returnCode = 1
-        return ''
-    }
-
-    whoami = () => {
-        return 'definitelynotroot\n'
-    }
-
-    sad = () => {
-        this.shell.returnCode = 4;
-        return 'Why would you want to use a different shell? 😭\n';
-    }
-
-    env = () => {
-        return Object.entries(this.shell.environment).map(([k, v]) => `${k}="${v}"`).join('\n');   
-    }
-
-    unalias = (arg) => {
-        if(!this.shell.aliases.hasOwnProperty(arg)) {
+  find = async (...args) => {
+    const find_r = (path) => (inode) => {
+      if (!inode.children) {
+        return [path + inode.name];
+      }
+      return [path + inode.name].concat(
+        Object.values(inode.children).map(find_r(path + inode.name))
+      );
+    };
+    let fileFilter = false;
+    let dirFilter = false;
+    while ((args[0] || "").startsWith("-")) {
+      const flag = args.shift();
+      switch (flag) {
+        case "-type":
+          const typeFilter = args.shift();
+          if (typeFilter === "f") {
+            fileFilter = true;
+          } else if (typeFilter === "d") {
+            dirFilter = true;
+          } else {
             this.shell.returnCode = 1;
-            return `no alias named '${arg}'`;
-        }
-        delete this.shell.aliases[arg];
-        return '';
+            throw new Error("unknown type flag\n");
+          }
+          break;
+      }
     }
+    let inode = this.fs.findFileNode(this.shell.cwd_p, args[0]);
+    if (!inode || inode.isFile) {
+      this.shell.returnCode = 1;
+      return "directory not found\n";
+    }
+    const base = fileFilter ? [] : ["."];
+    return base
+      .concat(
+        find_r("")(inode)
+          .flat(Infinity)
+          .filter(
+            (file) =>
+              (!fileFilter && !dirFilter) ||
+              (fileFilter && !file.endsWith("/")) ||
+              (dirFilter && file.endsWith("/"))
+          )
+      )
+      .join("\n");
+  };
 
-    exportVar = (arg) => {
-        const split = arg.split('=');
-        if (split.length < 2) {
-            this.shell.returnCode = 1;
-            return '';
-        }
-        const environmentVar = split.shift();
-        this.shell.environment[environmentVar] = split.join('');
-        return '';
+  ls = async (dir) => {
+    let inode = this.fs.findFileNode(this.shell.cwd_p, dir);
+    if (!inode || inode.isFile) {
+      this.shell.returnCode = 1;
+      return "directory not found\n";
     }
+    const base = ["."];
+    if (inode.parent) base.push("..");
+    return base.concat(Object.keys(inode.children)).join("\n");
+  };
 
-    alias = (arg) => {
-        const split = arg.split('=');
-        if (split.length < 2) {
-            this.shell.returnCode = 1;
-            return '';
-        }
-        const aliasName = split.shift();
-        this.shell.aliases[aliasName] = split.join('');
-        return '';
-    }
+  curl = async (url) => {
+    const resp = await window.fetch(url);
+    return await resp.text();
+  };
 
-    clear = () => {
-        // ehhrrm, well
-        return '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n';
-    }
+  trueProgram = () => {
+    this.shell.returnCode = 0;
+    return "";
+  };
 
-    cat = async (...files) => {
-        const printFile = async (file) => {
-            if(this.shell.returnCode) return;
-            return await this.fs.readFileAsync(this.shell.cwd_p, file);
-        }
-        if (this.shell.stdin) {
-            return this.shell.stdin;
-        } else {
-            return (await asyncMap(files.map(fileList => fileList.split('\n')).flat(), printFile)).join('\n');
-        }
-    }
+  falseProgram = () => {
+    this.shell.returnCode = 1;
+    return "";
+  };
 
-    xargs = async (...args) => {
-        if(!this.shell.stdin) {
-            return;
-        }
-        const command = args.shift();
-        const expandedArgs = [await asyncMap(args, expand)].flat(Infinity);
-        const allArgs = expandedArgs.concat(this.shell.stdin.split('\n'));
-    
-        this.shell.stdin = ''
-        return await this.commands[command](...allArgs);
-    }
-    welcome = () => {
-        return 'You\'ve discovered the hidden shell!\nWhat else is there to find? 🤔\n';
-    }
+  whoami = () => {
+    return "definitelynotroot\n";
+  };
 
-    sudo = () => {
-        this.shell.returnCode = 1;
-        return 'Username is not in the sudoers file. This incident will be reported\n';
-    }
+  sad = () => {
+    this.shell.returnCode = 4;
+    return "Why would you want to use a different shell? 😭\n";
+  };
 
-    pwd = () => {
-        return `${this.shell.cwd}\n`;
-    }
+  env = () => {
+    return Object.entries(this.shell.environment)
+      .map(([k, v]) => `${k}="${v}"`)
+      .join("\n");
+  };
 
-    echo = (...args) => {
-        let ending = '\n';
-        if(args.length && args[0] === '-n')  {
-            args.shift();
-            ending = '';
-        }
-        return args.join(" ") + ending;
+  unalias = (arg) => {
+    if (!this.shell.aliases.hasOwnProperty(arg)) {
+      this.shell.returnCode = 1;
+      return `no alias named '${arg}'`;
     }
+    delete this.shell.aliases[arg];
+    return "";
+  };
 
-}
+  exportVar = (arg) => {
+    const split = arg.split("=");
+    if (split.length < 2) {
+      this.shell.returnCode = 1;
+      return "";
+    }
+    const environmentVar = split.shift();
+    this.shell.environment[environmentVar] = split.join("");
+    return "";
+  };
+
+  alias = (arg) => {
+    const split = arg.split("=");
+    if (split.length < 2) {
+      this.shell.returnCode = 1;
+      return "";
+    }
+    const aliasName = split.shift();
+    this.shell.aliases[aliasName] = split.join("");
+    return "";
+  };
+
+  clear = () => {
+    // ehhrrm, well
+    return "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+  };
+
+  cat = async (...files) => {
+    const printFile = async (file) => {
+      if (this.shell.returnCode) return;
+      return await this.fs.readFileAsync(this.shell.cwd_p, file);
+    };
+    if (this.shell.stdin) {
+      return this.shell.stdin;
+    } else {
+      return (
+        await asyncMap(
+          files.map((fileList) => fileList.split("\n")).flat(),
+          printFile
+        )
+      ).join("\n");
+    }
+  };
+
+  xargs = async (...args) => {
+    if (!this.shell.stdin) {
+      return;
+    }
+    const command = args.shift();
+    const expandedArgs = [await asyncMap(args, expand)].flat(Infinity);
+    const allArgs = expandedArgs.concat(this.shell.stdin.split("\n"));
+
+    this.shell.stdin = "";
+    return await this.commands[command](...allArgs);
+  };
+  welcome = () => {
+    return "You've discovered the hidden shell!\nWhat else is there to find? 🤔\n";
+  };
+
+  sudo = () => {
+    this.shell.returnCode = 1;
+    return "Username is not in the sudoers file. This incident will be reported\n";
+  };
+
+  pwd = () => {
+    return `${this.shell.cwd}\n`;
+  };
+
+  echo = (...args) => {
+    let ending = "\n";
+    if (args.length && args[0] === "-n") {
+      args.shift();
+      ending = "";
+    }
+    return args.join(" ") + ending;
+  };
+};
--- _shell/filesystem.js
+++ _shell/filesystem.js
@@ -1,140 +1,139 @@
 const identity = (a) => a;
 
 module.exports = class FileSystem {
+  constructor() {
+    this.filesystem = {
+      children: {
+        "/": {
+          children: {
+            // 'about/': {
+            // children: {
+            //     'index.html': {
+            //         linkTo: identity,
+            //     }
+            // }
+            // },
+            // 'blog/': {
+            // children: {
+            //     'post-1.html': {
+            //         linkTo: (path) => path.replace('.html', '.md'),
+            //     }
+            // }
+            // }
+          },
+        },
+      },
+    };
+    this.indexFilesystem();
+  }
 
-    constructor() {
-
-        this.filesystem = {
-            children: {
-                '/': {
-                    children: {
-                        // 'about/': {
-                            // children: {
-                                //     'index.html': {
-                                    //         linkTo: identity,
-                                    //     }
-                                    // }
-                                    // },
-                                    // 'blog/': {
-                                        // children: {
-                                            //     'post-1.html': {
-                                                //         linkTo: (path) => path.replace('.html', '.md'),
-                                                //     }
-                                                // }
-                                                // }
-                                            }
-                                        }
-                                    }
-                                };
-                                this.indexFilesystem();
-                            }
-
-    indexFilesystem = () => {
-        const recursiveHelper = (system, parent = null) => {
-            Object.keys(system.children || []).forEach(key => {
-                if(typeof system.children[key] !== "object") {
-                    return;
-                }
-                recursiveHelper(system.children[key], system.children[key]);
-                system.children[key].parent = parent;
-                system.children[key].name = key;
-                if(key.endsWith('/')) {
-                    system.children[key].isDirectory = true;
-                }
-                else {
-                    system.children[key].isFile = true;
-                }
-            })
-            return system;
+  indexFilesystem = () => {
+    const recursiveHelper = (system, parent = null) => {
+      Object.keys(system.children || []).forEach((key) => {
+        if (typeof system.children[key] !== "object") {
+          return;
         }
-        this.filesystem = recursiveHelper(this.filesystem);
-    }
-    
-
-    
-    getFsRoot = () => {
-        return this.filesystem.children['/'];
-    }
-    
-    findFileNode = (cwd, path) => {
-        let inode = cwd;
-        if(path && path.startsWith('/')) {
-            inode = getFsRoot();
+        recursiveHelper(system.children[key], system.children[key]);
+        system.children[key].parent = parent;
+        system.children[key].name = key;
+        if (key.endsWith("/")) {
+          system.children[key].isDirectory = true;
+        } else {
+          system.children[key].isFile = true;
         }
-        if(path) {
-            try {
-                path.split('/').filter(p=>p).forEach(seg => {
-                    if(!seg || seg === '.') return;
-                    if(seg === '..') {
-                        inode = inode.parent || inode;
-                    }
-                    else {
-                        inode = inode.children[seg + '/'] || inode.children[seg];
-                    }
-                })
-            } catch(e) {
-                return undefined;
+      });
+      return system;
+    };
+    this.filesystem = recursiveHelper(this.filesystem);
+  };
+
+  getFsRoot = () => {
+    return this.filesystem.children["/"];
+  };
+
+  findFileNode = (cwd, path) => {
+    let inode = cwd;
+    if (path && path.startsWith("/")) {
+      inode = getFsRoot();
+    }
+    if (path) {
+      try {
+        path
+          .split("/")
+          .filter((p) => p)
+          .forEach((seg) => {
+            if (!seg || seg === ".") return;
+            if (seg === "..") {
+              inode = inode.parent || inode;
+            } else {
+              inode = inode.children[seg + "/"] || inode.children[seg];
             }
-        }
-        return inode;
+          });
+      } catch (e) {
+        return undefined;
+      }
     }
-    
-    readFileAsync = async (cwd, file) => {
-        let inode = findFileNode(cwd, file);
-        if(!inode) {
-            throw new Error('directory not found\n');
-        }
-        if(inode.isDirectory) {
-            throw new Error(`${inode.name} is a directory\n`);
-        }
-        
-        if(inode.cache) {
-            return inode.cache;
-        }
-        
-        // build full path
-        let path = inode.name
-        let t_inode = inode
-        while(t_inode.parent) {
-            path = t_inode.parent.name + path;
-            t_inode = t_inode.parent;
-        }
-        
-        // no one has to know that cat makes a network call...
-        const resp = await window.fetch(inode.linkTo(path));
-        const body = await resp.text();
-        inode.cache = body;
-        return body;
+    return inode;
+  };
+
+  readFileAsync = async (cwd, file) => {
+    let inode = findFileNode(cwd, file);
+    if (!inode) {
+      throw new Error("directory not found\n");
     }
-    
-    initFilesystem = (xml) => {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xml,"text/xml");
-        const urls = xmlDoc.getElementsByTagName('url');
-        let pages = [];
-        for(let i = 0; i < urls.length; ++i) {
-            pages.push(urls[i].getElementsByTagName('loc')[0].innerHTML);
-        }
-        pages = pages.filter(p => p !== '/').map(page => {
-            let newPage = page.replace('.html', '.md');
-            if(newPage.endsWith('/')) newPage += 'index.md';
-            return newPage;
-        })
-        pages.forEach(page => {
-            let inode = this.filesystem.children['/'];
-            const segs = page.split('/').filter(p => p);
-            for(let i = 0; i < segs.length - 1; ++i) {
-                if(inode.children.hasOwnProperty(segs[i] + '/')) {
-                    inode = inode.children[segs[i]+'/'];
-                    continue;
-                }
-                inode.children[segs[i] + '/'] = { children: {} }
-                inode = inode.children[segs[i] + '/']
-            }
-            // Link final node
-            inode.children[segs[segs.length - 1]] = { linkTo: identity }
-        });
-        // Re-index filesystem
-        this.indexFilesystem();
+    if (inode.isDirectory) {
+      throw new Error(`${inode.name} is a directory\n`);
     }
-}
+
+    if (inode.cache) {
+      return inode.cache;
+    }
+
+    // build full path
+    let path = inode.name;
+    let t_inode = inode;
+    while (t_inode.parent) {
+      path = t_inode.parent.name + path;
+      t_inode = t_inode.parent;
+    }
+
+    // no one has to know that cat makes a network call...
+    const resp = await window.fetch(inode.linkTo(path));
+    const body = await resp.text();
+    inode.cache = body;
+    return body;
+  };
+
+  initFilesystem = (xml) => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xml, "text/xml");
+    const urls = xmlDoc.getElementsByTagName("url");
+    let pages = [];
+    for (let i = 0; i < urls.length; ++i) {
+      pages.push(urls[i].getElementsByTagName("loc")[0].innerHTML);
+    }
+    pages = pages
+      .filter((p) => p !== "/")
+      .map((page) => {
+        let newPage = page.replace(".html", ".md");
+        if (newPage.endsWith("/")) newPage += "index.md";
+        return newPage;
+      });
+    pages.forEach((page) => {
+      let inode = this.filesystem.children["/"];
+      const segs = page.split("/").filter((p) => p);
+      for (let i = 0; i < segs.length - 1; ++i) {
+        if (inode.children.hasOwnProperty(segs[i] + "/")) {
+          inode = inode.children[segs[i] + "/"];
+          continue;
+        }
+        inode.children[segs[i] + "/"] = { children: {} };
+        inode = inode.children[segs[i] + "/"];
+      }
+      // Link final node
+      inode.children[segs[segs.length - 1]] = { linkTo: identity };
+    });
+    // Re-index filesystem
+    this.indexFilesystem();
+  };
+};
